e6b1d55d99b709762562ad6547e81a9a94da7a3b
patrick681 <kamaupatrick332@gmail.com>
2025-04-18 21:53:58 +0300

    ,

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import blogData from "../data/blog";
 import Header from './Header'; // adjust the path if needed
 import About from './About';
 import ArticleList from './ArticleList';
+
 const blogPosts = [
   {
     id: 1,
@@ -25,14 +26,9 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-            <Header name="My Awesome Blog" />
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-      <About 
-        image="https://your-image-url.com/logo.png" 
-        about="Welcome to my blog where I share everything about tech and creativity!" 
-      />
-      <ArticleList posts={blogPosts} />
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
